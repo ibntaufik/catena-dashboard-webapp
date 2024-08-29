ef20c00c1039ef5bb591cb27e38ee5d1d22ac686
Faisal Abdillah <faisal@popbox.asia>
2024-08-29 14:50:53 +0700

    update master

--- resources/views/layout/master.blade.php
+++ resources/views/layout/master.blade.php
@@ -4,9 +4,9 @@
   <meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <meta name="description" content="Responsive Laravel Admin Dashboard Template based on Bootstrap 5">
-	<meta name="author" content="NobleUI">
-	<meta name="keywords" content="nobleui, bootstrap, bootstrap 5, bootstrap5, admin, dashboard, template, responsive, css, sass, html, laravel, theme, front-end, ui kit, web">
+  <meta name="description" content="Catena Dashboard">
+	<meta name="author" content="Cognizest">
+	<meta name="keywords" content="catena, bootstrap, bootstrap 5, bootstrap5, admin, dashboard, template, responsive, css, sass, html, laravel, theme, front-end, ui kit, web">
 
   <title>Catena Dashboard</title>
 
