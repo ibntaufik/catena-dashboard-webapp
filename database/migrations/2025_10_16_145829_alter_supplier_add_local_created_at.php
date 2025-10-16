66e963d4a1159a84a7683ec44ef29d38d183dfa6
Faisal <faisal@cognizest.com>
2025-10-16 15:02:59 +0700

    Create 2025_10_16_145829_alter_supplier_add_local_created_at.php

---
+++ database/migrations/2025_10_16_145829_alter_supplier_add_local_created_at.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    protected $table = "master_supplier";
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        if(Schema::hasTable($this->table)){
+            Schema::table($this->table, function (Blueprint $table) {
+                $table->timestamp('local_created_at')->nullable()->index()->after("verification_status");
+            });
+        }
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table($this->table, function (Blueprint $table) {
+            $table->dropColumn(['local_created_at']);
+        });
+    }
+};
