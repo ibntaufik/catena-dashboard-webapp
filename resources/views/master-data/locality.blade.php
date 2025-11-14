6b1b493d6180c6af3df19fbdd2526750d982bf9a
Faisal <faisal@cognizest.com>
2025-11-14 12:56:33 +0700

    Update locality.blade.php

--- resources/views/master-data/locality.blade.php
+++ resources/views/master-data/locality.blade.php
@@ -21,7 +21,7 @@
 
 <div class="row">
   <div class="col-md-12 grid-margin stretch-card">
-    <div class="card">
+    <div class="card d-none">
       <div class="card-body">
           <h6 class="card-title">Create Location</h6>
           <div class="row">
@@ -98,7 +98,7 @@
   <div class="col-md-12 grid-margin stretch-card">
     <div class="card">
       <div class="card-body">
-        <h6 class="card-title">List of locations</h6>
+        <h6 class="card-title">List of locality</h6>
         <div class="table-responsive">
           <table id="gridDataTable" class="table">
             <thead>
